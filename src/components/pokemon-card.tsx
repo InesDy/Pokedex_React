629dc8ae3b6c7a52365b40211dd54ada8112b4d6
Ines Dyll <inesdyll1@gmail.com>
2020-08-12 15:33:51 +0200

    splitted out formatDate and formatType in new files

--- src/components/pokemon-card.tsx
+++ src/components/pokemon-card.tsx
@@ -1,5 +1,7 @@
 import React, { FunctionComponent, useState } from "react";
 import Pokemon from "../models/pokemon";
+import "../helpers/format-date";
+import "../helpers/format-type";
 import "./pokemon-card.css";
 
 type Props = {
@@ -19,55 +21,6 @@ const PokemonCard: FunctionComponent<Props> = ({
 
   const hideBorder = () => {
     setColor("#f5f5f5"); //Back to grey border (picked as initial)
-  };
-
-  const formatDate = (date: Date): string => {
-    return `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`;
-  };
-
-  const formatType = (type: string): string => {
-    let color: string;
-
-    switch (type) {
-      case "Feu":
-        color = "red lighten-1";
-        break;
-      case "Eau":
-        color = "blue lighten-1";
-        break;
-      case "Plante":
-        color = "green lighten-1";
-        break;
-      case "Insecte":
-        color = "brown lighten-1";
-        break;
-      case "Normal":
-        color = "grey lighten-3";
-        break;
-      case "Vol":
-        color = "blue lighten-3";
-        break;
-      case "Poison":
-        color = "deep-purple accent-1";
-        break;
-      case "Fée":
-        color = "pink lighten-4";
-        break;
-      case "Psy":
-        color = "deep-purple darken-2";
-        break;
-      case "Electrik":
-        color = "lime accent-1";
-        break;
-      case "Combat":
-        color = "deep-orange";
-        break;
-      default:
-        color = "grey";
-        break;
-    }
-
-    return `chip ${color}`;
   };
 
   return (
