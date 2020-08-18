18baf0769fee68276d52634baf5878c903b6f9a0
Ines Dyll <inesdyll1@gmail.com>
2020-08-18 11:43:02 +0200

    added router

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,32 @@
-import React, { FunctionComponent } from 'react';
-import PokemonList from './pages/pokemon-list';
+import React, { FunctionComponent } from "react";
+import PokemonList from "./pages/pokemon-list";
+import PokemonDetail from "./pages/pokemon-detail";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-import './App.css';
+import "./App.css";
 
 const App: FunctionComponent = () => {
-
   return (
-  <PokemonList />
-  )
- }
+    <Router>
+      <div>
+        {/* Nav for all the pages*/}
+        <nav>
+          <div className="nav-wrapper teal">
+            <Link to="/" className="brand-logo center">
+              {" "}
+              Pokédex{" "}
+            </Link>
+          </div>
+        </nav>
+        {/* Routes handler */}
+        <Switch>
+          <Route exact path="/" component={PokemonList} />
+          <Route exact path="/pokemons" component={PokemonList} />
+          <Route exact path="/pokemons/:id" component={PokemonDetail} />
+        </Switch>
+      </div>
+    </Router>
+  );
+};
 
 export default App;
